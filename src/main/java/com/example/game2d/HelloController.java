d8687288556935bc94e5e2f638c6d32dd6485546
Ziyaddin01 <ziya.huseynov200101@gmail.com>
2023-10-25 02:30:33 +0400

    3 Hisse Enemy yaratdim

--- src/main/java/com/example/game2d/HelloController.java
+++ src/main/java/com/example/game2d/HelloController.java
@@ -17,7 +17,7 @@ public class HelloController {
     private URL location;
 
     @FXML
-    private ImageView bg1, bg2, player;
+    private ImageView bg1, bg2, player, enemy;
 
     private final int BG_WIDTH = 707;
 
@@ -58,6 +58,13 @@ public class HelloController {
         bgTwoTransition.setFromX(0);
         bgTwoTransition.setToX(BG_WIDTH * -1);
         bgTwoTransition.setInterpolator(Interpolator.LINEAR);
+
+        TranslateTransition enemyTransition = new TranslateTransition(Duration.millis(3500), enemy);
+        enemyTransition.setFromX(0);
+        enemyTransition.setToX(BG_WIDTH * -1 - 100);
+        enemyTransition.setInterpolator(Interpolator.LINEAR);
+        enemyTransition.setCycleCount(Animation.INDEFINITE);
+        enemyTransition.play();
 
         parallelTransition = new ParallelTransition(bgOneTransition, bgTwoTransition);
         parallelTransition.setCycleCount(Animation.INDEFINITE);
